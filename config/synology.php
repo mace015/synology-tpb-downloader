b0b13975ef8342418d22420af9b57652549d3536
Mace Muilman <mace@macemuilman.nl>
2017-01-04 23:46:35 +0100

    Added synology config data to establish a connection between the application and the NAS.

---
+++ config/synology.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+	'host' 		=> env('SYNOLOGY_HOST', ''),
+
+	'port' 		=> env('SYNOLOGY_PORT', '5001'),
+
+	'protocol' 	=> env('SYNOLOGY_PROTOCOL', 'https'),
+
+	'user' 		=> env('SYNOLOGY_USER', ''),
+
+	'password' 	=> env('SYNOLOGY_PASSWORD', ''),
+
+];
