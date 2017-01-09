fbfee5b3563d29809d08a5769049c009c42eb6a4
Mace Muilman <mace@macemuilman.nl>
2017-01-09 20:47:45 +0100

    Large styling overhaul, implemented the Bulma CSS framework to improve the overal look of the application.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,68 +3,101 @@
 		<title> Synology TPB Downloader </title>
 
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
 	</head>
 	<body>
 
-		<h1> Synology TPB Downloader </h1>
-		
-		{!! Form::open() !!}
+		@include('partials.menu')
 
-			<input name="search" value="{{ ((isset($search))? $search : '') }}" />
-			<button type="submit" class="green-button"> Search </button>
+		<section class="hero is-primary">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title">
+						Synology TPB Downloader
+					</h1>
+				</div>
+			</div>
+		</section>
 
-		{!! Form::close() !!}
+		<section class="section">
+			<div class="container">
 
-		<table>
-			<thead>
-				<th> Name </th>
-				<th> Status </th>
-				<th> Delete </th>
-			</thead>
-			<tbody>
-				@if(count($downloads->tasks) == 0)
-					<tr>
-						<td colspan="3"> No active downloads </td>
-					</tr>
-				@endif
-				@foreach($downloads->tasks as $download)
-					<tr>
-						<td> {{ $download->title }} </td>
-						<td> {{ ucfirst($download->status) }} </td>
-						<td> <a href="{{ URL::route('download.delete', $download->id) }}"> Delete </a> </td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
+				<h1 class="title">Active downloads</h1>
 
-		<br>
+				<table class="table is-bordered is-striped">
+					<thead>
+						<th> Name </th>
+						<th> Status </th>
+						<th> Delete </th>
+					</thead>
+					<tbody>
+						@if(count($downloads->tasks) == 0)
+							<tr>
+								<td colspan="3"> No active downloads </td>
+							</tr>
+						@endif
+						@foreach($downloads->tasks as $download)
+							<tr>
+								<td> {!! $download->title !!} </td>
+								<td> {{ ucfirst($download->status) }} </td>
+								<td> 
+									<a class="button is-danger" href="{{ URL::route('download.delete', $download->id) }}"> 
+										<span class="icon is-small"> <i class="fa fa-trash"></i> </span>
+										<span> Delete </span>
+									</a> 
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
 
-		<table>
-			<thead>
-				<th> Name </th>
-				<th> Seeders </th>
-				<th> Download </th>
-			</thead>
-			<tbody>
-				@if(!isset($results) || count($results) == 0)
-					<tr> <td colspan="4" class="center"> @if(!empty($search)) No results found @else No search query @endif </td> </tr>
-				@else
-					@foreach($results as $result)
-						<tr>
-							<td> {{ $result->getName() }} </td>
-							<td> {{ $result->getSeeders() }} </td>
-							<td>
-								{{ Form::open(['action' => 'DownloadController@download']) }}
-									<input type="hidden" name="magnet" value="{{ $result->getMagnetUrl() }}">
-									<button type="submit"> Download </button>
-								{{ Form::close() }}
-							</td>
-						</tr>
-					@endforeach
-				@endif
-			</tbody>
-		</table>
+				<h1 class="title">Start a new download</h1>
+
+				{!! Form::open() !!}
+
+					<label class="label">Search</label>
+					<p class="control has-addons">
+						<input class="input is-expanded" name="search" value="{{ ((isset($search))? $search : '') }}" />
+
+						<button type="submit" class="button is-primary">
+							<span class="icon is-small"> <i class="fa fa-search"></i> </span>
+							<span> Search </span> 
+						</button>
+					</p>
+
+				{!! Form::close() !!}
+
+				<table class="table is-bordered is-striped">
+					<thead>
+						<th> Name </th>
+						<th> Seeders </th>
+						<th> Download </th>
+					</thead>
+					<tbody>
+						@if(!isset($results) || count($results) == 0)
+							<tr> <td colspan="4" class="center"> @if(!empty($search)) No results found @else No search query @endif </td> </tr>
+						@else
+							@foreach($results as $result)
+								<tr>
+									<td> {{ $result->getName() }} </td>
+									<td> {{ $result->getSeeders() }} </td>
+									<td>
+										{{ Form::open(['action' => 'DownloadController@download']) }}
+											<input type="hidden" name="magnet" value="{{ $result->getMagnetUrl() }}">
+											<button type="submit" class="button is-primary">
+												<span class="icon is-small"> <i class="fa fa-download"></i> </span>
+												<span> Download </span> 
+											</button>
+										{{ Form::close() }}
+									</td>
+								</tr>
+							@endforeach
+						@endif
+					</tbody>
+				</table>
+
+			</div>
+		</section>
 
 	</body>
 </html>
