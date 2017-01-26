31ff9af351d98707767fa212d4d5fdcd10970823
Mace Muilman <mace@macemuilman.nl>
2017-01-26 22:45:00 +0100

    Fixed iOS mobile styling.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,31 +4,33 @@
 
 	<h1 class="title">Active downloads</h1>
 
-	<table class="table is-bordered is-striped">
-		<thead>
-			<th> Name </th>
-			<th> Status </th>
-			<th> Delete </th>
-		</thead>
-		<tbody>
-			<tr v-show="is_loading_downloads">
-				<td colspan="4" class="center"> <strong> Loading downloads, one moment please... </strong> </td>
-			</tr>
-			<tr v-show="downloads.length == 0 && !is_loading_downloads">
-				<td colspan="3"> No active downloads </td>
-			</tr>
-			<tr v-show="!is_loading_downloads" v-for="download in downloads">
-				<td> @{{ download.title }} </td>
-				<td> @{{ download.status }}</td>
-				<td>
-					<a class="button is-danger is-pulled-right" @click="removeDownload(download.id, $event)">
-						<span class="icon is-small"> <i class="fa fa-trash"></i> </span>
-						<span> Delete </span>
-					</a>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+	<div class="table-container">
+		<table class="table is-bordered is-striped">
+			<thead>
+				<th> Name </th>
+				<th> Status </th>
+				<th> Delete </th>
+			</thead>
+			<tbody>
+				<tr v-show="is_loading_downloads">
+					<td colspan="4" class="center"> <strong> Loading downloads, one moment please... </strong> </td>
+				</tr>
+				<tr v-show="downloads.length == 0 && !is_loading_downloads">
+					<td colspan="3"> No active downloads </td>
+				</tr>
+				<tr v-show="!is_loading_downloads" v-for="download in downloads">
+					<td> @{{ download.title }} </td>
+					<td> @{{ download.status }}</td>
+					<td>
+						<a class="button is-danger is-pulled-right" @click="removeDownload(download.id, $event)">
+							<span class="icon is-small"> <i class="fa fa-trash"></i> </span>
+							<span> Delete </span>
+						</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 
 	<h1 class="title">Start a new download</h1>
 
@@ -42,30 +44,32 @@
 		</button>
 	</p>
 
-	<table class="table is-bordered is-striped">
-		<thead>
-			<th> Name </th>
-			<th> Seeders </th>
-			<th> Download </th>
-		</thead>
-		<tbody>
-			<tr v-show="is_loading_results">
-				<td colspan="4"> <strong> Scraping TPB for results, one moment please... </strong> </td>
-			</tr>
-			<tr v-show="results.length == 0 && !is_loading_results">
-				<td colspan="4"> No results found! </td>
-			</tr>
-			<tr v-show="!is_loading_results" v-for="result in results">
-				<td> @{{ result.name }} </td>
-				<td> @{{ result.seeders }} </td>
-				<td>
-					<button type="submit" class="button is-primary is-pulled-right" @click="startDownload(result.magnet, $event)">
-						<span class="icon is-small"> <i class="fa fa-download"></i> </span>
-						<span> Download </span>
-					</button>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+	<div class="table-container">
+		<table class="table is-bordered is-striped">
+			<thead>
+				<th> Name </th>
+				<th> Seeders </th>
+				<th> Download </th>
+			</thead>
+			<tbody>
+				<tr v-show="is_loading_results">
+					<td colspan="4"> <strong> Scraping TPB for results, one moment please... </strong> </td>
+				</tr>
+				<tr v-show="results.length == 0 && !is_loading_results">
+					<td colspan="4"> No results found! </td>
+				</tr>
+				<tr v-show="!is_loading_results" v-for="result in results">
+					<td> @{{ result.name }} </td>
+					<td> @{{ result.seeders }} </td>
+					<td>
+						<button type="submit" class="button is-primary is-pulled-right" @click="startDownload(result.magnet, $event)">
+							<span class="icon is-small"> <i class="fa fa-download"></i> </span>
+							<span> Download </span>
+						</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 		<title> Synology TPB Downloader </title>
 
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
